d8d47c9c94c07713f1c24ec41e94ba113935f732
mat@blocknot.es <mat@blocknot.es>
2018-04-25 15:47:59 +0200

    Test Json OJ

--- test_json_oj/app/main.rb
+++ test_json_oj/app/main.rb
@@ -3,51 +3,96 @@ require 'grape-entity'
 require 'oj'
 require 'pry'
 
+module GrapeEntitySerializable
+  def to_json( options = nil )
+    Oj.dump serializable_hash.to_h, mode: :compat
+  end
+end
+
+module WithGrapeEntity
+  def entity
+    @entity ||= self.class::Entity.new self
+  end
+
+  def to_json( options = nil )
+    entity.to_json options
+  end
+end
+
 module MyAPI
-  module OjSerialize
-    def to_json( options = nil )
-      Oj.dump serializable_hash.to_h, mode: :compat
-    end
+  # --- Method 1
+  class PostEntity < Grape::Entity
+    include GrapeEntitySerializable
+
+    expose :id
+    expose :title
+    expose :content
   end
-end
 
-class MyAPI::PostEntity < Grape::Entity
-  include MyAPI::OjSerialize
+  # --- Method 2
+  class Post
+    include WithGrapeEntity
 
-  expose :id
-  expose :title
-  expose :content
-end
+    attr_accessor :id, :title, :content, :dt
 
-class MyAPI::Main < Grape::API
-  version 'v1', using: :path
-  format :json
-  prefix :api
-  rescue_from :all
-
-  resource :posts do
-    get do  # /api/v1/posts
-      posts = [
-        OpenStruct.new({ id: 1, title: 'A test', content: 'Just some content', dt: Date.today }),
-        OpenStruct.new({ id: 2, title: 'Another test', content: 'Some other content', dt: Date.yesterday }),
-      ]
-      MyAPI::PostEntity.represent posts, only: [:id, :title]
-    end
-
-    params do
-      requires :id, type: Integer, desc: 'The id'
-    end
-    get ':id' do  # /api/v1/posts/:id
-      post = case params[:id]
-        when 1 then OpenStruct.new({ id: 1, title: 'A test', content: 'Just some content', dt: Date.today })
-        when 2 then OpenStruct.new({ id: 2, title: 'Another test', content: 'Some other content', dt: Date.yesterday })
-        else nil
+    def initialize( attributes = {} )
+      attributes.each do |k, v|
+        send "#{k}=", v
       end
-      MyAPI::PostEntity.represent post
+    end
+
+    class Entity < Grape::Entity
+      include GrapeEntitySerializable
+
+      expose :id
+      expose :title
+      expose :content
     end
   end
 
-  route :any, '*path' do
-    error!( 'not found', 404 )
+  # ---
+  class Main < Grape::API
+    version 'v1', using: :path
+    format :json
+    prefix :api
+    rescue_from :all
+
+    resource :posts do
+      get do  # /api/v1/posts
+        if params[:m2]  # /api/v1/posts?m2=1
+          puts '>>> Method 2'
+          posts = [
+            Post.new({ id: 1, title: 'A test', content: 'Just some content', dt: Date.today }),
+            Post.new({ id: 2, title: 'Another test', content: 'Some other content', dt: Date.yesterday }),
+          ]
+          present posts, only: [:id, :title]
+        else
+          puts '>>> Method 1'
+          posts = [
+            OpenStruct.new({ id: 1, title: 'A test', content: 'Just some content', dt: Date.today }),
+            OpenStruct.new({ id: 2, title: 'Another test', content: 'Some other content', dt: Date.yesterday }),
+          ]
+          MyAPI::PostEntity.represent posts, only: [:id, :title]
+        end
+      end
+
+      params do
+        requires :id, type: Integer, desc: 'The id'
+      end
+      get ':id' do  # /api/v1/posts/:id
+        case params[:id]
+        when 1
+          MyAPI::PostEntity.represent OpenStruct.new({ id: 1, title: 'A test', content: 'Just some content', dt: Date.today })
+        when 2
+          MyAPI::Post.new({ id: 2, title: 'Another test', content: 'Some other content', dt: Date.yesterday })
+        else
+          {}
+        end
+      end
+    end
+
+    route :any, '*path' do
+      error!( 'not found', 404 )
+    end
   end
 end
