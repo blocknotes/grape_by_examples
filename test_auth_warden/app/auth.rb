7a9e0ad89d54dee4e88c0075f30aa8bc612974dc
mat@blocknot.es <mat@blocknot.es>
2018-05-01 08:32:21 +0200

    Test auth Warden

--- test_auth_warden/app/auth.rb
+++ test_auth_warden/app/auth.rb
@@ -1,59 +1,62 @@
 require 'securerandom'
 
-module Auth
+class Auth
   TOKEN_DURATION = 30.second.freeze
 
-  def Auth.init( app )
-    app.use Warden::Manager do |manager|
-      manager.default_strategies :token
-      manager.failure_app = ->( env ) {
-        [401, { 'Content-Type' => 'application/json' }, [{ error: env['warden'].message || 'Unauthorized' }.to_json]]
-      }
-    end
-
-    Warden::Strategies.add(:token) do
-      def valid?
-        env['HTTP_AUTH_TOKEN'] || env['rack.request.query_hash']['AUTH_TOKEN']
+  class << self
+    def init( app )
+      app.use Warden::Manager do |manager|
+        manager.default_strategies :token
+        manager.failure_app = ->( env ) {
+          [401, { 'Content-Type' => 'application/json' }, [{ error: env['warden'].message || 'Unauthorized' }.to_json]]
+        }
       end
 
-      def authenticate!
-        token = valid?
-        user = User.find( token: token )
-        if user
-          if Time.now < (user.token_created_at + TOKEN_DURATION)
-            success!(user)
+      Warden::Strategies.add(:token) do
+        def valid?
+          env['HTTP_AUTH_TOKEN'] || env['rack.request.query_hash']['AUTH_TOKEN']
+        end
+
+        def authenticate!
+          token = valid?
+          user = User.find( token: token )
+          if user
+            if Time.now < (user.token_updated_at + TOKEN_DURATION)
+              User.update user.id, { token_updated_at: Time.now }
+              success!(user)
+            else
+              User.update user.id, { token: nil }
+              fail!('Token expired')
+            end
           else
-            User.update user.id, { token: nil }
-            fail!('Token expired')
+            fail!('Could not log in')
           end
-        else
-          fail!('Could not log in')
         end
       end
     end
-  end
 
-  def Auth.sign_in( user_data )
-    user = User.find user_data
-    if user
-      user = User.update user.id, { token: SecureRandom.urlsafe_base64(80, false), token_created_at: Time.now }
-      return user.to_h if user
+    def sign_in( user_data )
+      user = User.find user_data
+      if user
+        user = User.update user.id, { token: SecureRandom.urlsafe_base64(80, false), token_updated_at: Time.now }
+        return user.to_h if user
+      end
     end
-  end
 
-  def Auth.sign_out( user_data )
-    user = User.find user_data
-    if user
-      user = User.update user.id, { token: nil }
-      return user.to_h if user
+    def sign_out( user_data )
+      user = User.find user_data
+      if user
+        user = User.update user.id, { token: nil }
+        return user.to_h if user
+      end
     end
-  end
 
-  def Auth.sign_up( user_data )
-    user = User.create user_data
-    if user
-      user = User.update user.id, { token: SecureRandom.urlsafe_base64(80, false), token_created_at: Time.now }
-      return user.to_h if user
+    def sign_up( user_data )
+      user = User.create user_data
+      if user
+        user = User.update user.id, { token: SecureRandom.urlsafe_base64(80, false), token_updated_at: Time.now }
+        return user.to_h if user
+      end
     end
   end
 end
---
+++ test_auth_warden/app/user.rb
@@ -0,0 +1,48 @@
+USERS = [
+  {
+    id: 1,
+    username: 'Mat'
+  },
+  {
+    id: 2,
+    username: 'Jim'
+  }
+]  # SAMPLE DATA
+
+class User
+  # attr_accessor :id
+
+  class << self
+    def create(attributes)
+      return nil unless attributes[:username]
+      user = find username: attributes[:username]
+      if user
+        nil
+      else
+        user = {
+          id: USERS.count + 1,
+          username: attributes[:username]
+        }
+        USERS << user
+        OpenStruct.new(user)
+      end
+    end
+
+    def find(attributes)
+      USERS.each do |user|
+        return OpenStruct.new(user) if (attributes.to_a - user.to_a).empty?
+      end
+      nil
+    end
+
+    def update(id, attributes)
+      USERS.each do |user|
+        if user[:id] == id
+          user.merge! attributes.dup.symbolize_keys
+          return OpenStruct.new(user)
+        end
+      end
+      nil
+    end
+  end
+end
